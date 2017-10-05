0e78d499f49dadd7111ed330ebba90f7954dba6c
Fan <ccfan.dev@gmail.com>
2017-10-05 15:09:52 +0800

    vue.js my-blog:实现路由

---
+++ vue.js/my-blog/src/components/BlogHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <!-- exact的作用是使其可以切换选中状态 -->
+        <router-link to="/" exact>博客</router-link>
+        <router-link to="/add" exact>写博客</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+
+  export default {
+    name: "blog-header"
+  }
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    text-align: center;
+    margin: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 20px;
+  }
+
+  /* 最终的页面会产生 a 标签 */
+  a {
+    color: #fff;
+    text-decoration: none;
+    padding: 12px;
+    border-radius: 5px;
+  }
+
+  nav {
+    background: crimson;
+    padding: 20px 0;
+    margin-bottom: 40px;
+  }
+
+  .router-link-active {
+    background: rgba(255, 255, 255, 0.8);
+    color: #444;
+  }
+</style>
+
--- vue.js/my-blog/src/main.js
+++ vue.js/my-blog/src/main.js
@@ -2,30 +2,33 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import App from './App'
+import Routes from './routes'
 
 Vue.config.productionTip = false
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
 
 
-Vue.directive('rainbow',{
-  bind(el,binding,vnode){
-    el.style.color = "#" + Math.random().toString(16).slice(2,8);
+Vue.directive('rainbow', {
+  bind(el, binding, vnode) {
+    el.style.color = "#" + Math.random().toString(16).slice(2, 8);
   }
 })
 
-Vue.directive('theme',{
-  bind(el,binding,vnode){
-    if (binding.value == 'wide'){
+Vue.directive('theme', {
+  bind(el, binding, vnode) {
+    if (binding.value == 'wide') {
       el.style.maxWidth = '1260px';
-    }else if (binding.value == 'narrow') {
+    } else if (binding.value == 'narrow') {
       el.style.maxWidth = '560px';
     } else {
       el.style.maxWidth = '800px';
     }
 
-    if (binding.arg == 'column'){
+    if (binding.arg == 'column') {
       el.style.background = "#67c";
       el.style.padding = '20px';
     }
@@ -34,13 +37,20 @@ Vue.directive('theme',{
 
 
 //
-Vue.filter("to-uppercase",function (value) {
+Vue.filter("to-uppercase", function (value) {
   return value.toUpperCase();
 })
 
 
-Vue.filter("snippet",function (value) {
-  return value.slice(0,100) + " ...";
+Vue.filter("snippet", function (value) {
+  return value.slice(0, 100) + " ...";
+})
+
+
+//创建路由
+const router = new VueRouter({
+  routes: Routes,
+  mode: "history"
 })
 
 
@@ -48,5 +58,6 @@ Vue.filter("snippet",function (value) {
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: {App},
+  router:router   //使用路由
 })
