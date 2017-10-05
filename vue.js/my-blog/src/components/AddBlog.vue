fd30b0c3d3ff708a9bfd5cfd4b67f55e75fc7463
Fan <ccfan.dev@gmail.com>
2017-10-05 09:55:34 +0800

    vue.js my-bolg: 完成自定义指令

--- vue.js/my-blog/src/components/AddBlog.vue
+++ vue.js/my-blog/src/components/AddBlog.vue
@@ -1,25 +1,139 @@
 <template>
-  <div class="add-blog">
+  <div id="add-blog">
     <h2>添加博客</h2>
-    <form>
+    <form v-if="!submited">
       <label>博客标题</label>
-      <input type="text" v-model="">
+      <input type="text" v-model="blog.title" required>
+      <label>博客内容</label>
+      <textarea v-model="blog.content" rows="15" cols="80"></textarea>
+
+      <div id="checkboxes">
+        <label>Vue.js</label>
+        <input type="checkbox"  value="Vue.js" v-model="blog.categories">
+        <label>Node.js</label>
+        <input type="checkbox"  value="Node.js" v-model="blog.categories">
+        <label>React.js</label>
+        <input type="checkbox"  value="React.js" v-model="blog.categories">
+        <label>Angular4.js</label>
+        <input type="checkbox"  value="Angular4.js" v-model="blog.categories">
+      </div>
+
+      <label>作者：</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors">{{author}}</option>
+      </select>
+      <!-- prevent 可以阻止刷新页面 -->
+      <button v-on:click.prevent="post">添加博客</button>
     </form>
+
+
+    <div v-if="submited">
+      <h3>您的博客已经成功添加</h3>
+    </div>
+
+<hr>
+
+    <div id="preview">
+      <h3>博客总览</h3>
+      <p>博客标题：{{blog.title}}</p>
+      <p>博客内容：</p>
+      <p>{{blog.content}}</p>
+      <p>博客分类</p>
+      <ul>
+        <li v-for="category in blog.categories">{{category}}</li>
+      </ul>
+      <p>作者： {{blog.author}}</p>
+    </div>
+
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'add-blog',
-    data() {
-      return {
-
-      }
+export default {
+  name: 'add-blog',
+  data() {
+    return {
+      blog: {
+        title: '',
+        content: '',
+        categories:[],
+        author:""
+      },
+      authors:[
+        "jian",
+        "fan",
+        "chang"
+      ],
+      submited: false
+    }
+  },
+  methods:{
+    post:function () {
+      this.$http.post("http://jsonplaceholder.typicode.com/posts",
+        { //post给服务器的对象；根据上述地址返回的 json格式即为需要post的数据格式(数据库保存的格式）
+          title:this.blog.title,
+          body:this.blog.content,
+          userId: 1
+        })
+        .then(function (data) { //then表示post成功后
+          console.log(data);
+          this.submited = true;
+        });
     }
   }
+}
 </script>
 
-
 <style scoped>
+#add-blog *{
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
 
+  #add-blog{
+    margin: 20px auto;
+    max-width: 600px;
+    padding:20px;
+  }
+
+  label{
+    display: block;
+    margin:20px 0 10px;
+  }
+
+  input[type="text"],textarea,select{
+    display: block;
+    width:100%;
+    padding:8px;
+  }
+
+  #checkboxes label{
+    display: inline-block;
+    margin-left:10px;
+  }
+
+  button{
+    display: block;
+    margin:20px 0;
+    background: crimson;
+    color: #fff;
+    border: 0;
+    padding: 8px;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 5px;
+    font-size:18px;
+    cursor: pointer;
+  }
+
+  #preview{
+    padding: 10px 20px;
+    border: 1px dotted #ccc;
+    margin: 30px 0;
+  }
+
+  h3{
+    margin-top:10px;
+  }
 </style>
---
+++ vue.js/my-blog/src/components/ShowBlogs.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="show-blogs" v-theme:column="'wide'">
+    <h1>博客总览</h1>
+    <div v-for="blog in blogs" class="single-blog">
+      <h2 v-rainbow>{{blog.title}}</h2>
+      <article>
+        {{blog.body}}
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'show-blogs',
+    data() {
+      return {
+        blogs: []
+      }
+    },
+    created() { //在此请求数据
+//      this.$http.get('http://jsonplaceholder.typicode.com/posts')
+      this.$http.get('../../static/blogs') // 下载好的json数据需放到 static 文件夹下才能访问
+        .then(function (data) {
+//          console.log(data);
+          this.blogs = data.body.slice(0, 10);
+//          console.log(this.blogs);
+        })
+    }
+  }
+</script>
+
+
+<style>
+  #show-blogs {
+    max-width: 800px;
+    margin: 0 auto;
+  }
+
+  .single-blog {
+    padding: 20px;
+    margin:20px 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    background: #eee;
+  }
+</style>
