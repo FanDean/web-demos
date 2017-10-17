f44c09fdb1fd8ea5295c161b20da5ab5b07203fe
Fan <ccfan.dev@gmail.com>
2017-10-17 16:32:58 +0800

    项目node.js>blog: 前台内容分页功能

--- node.js/blog/routers/main.js
+++ node.js/blog/routers/main.js
@@ -2,20 +2,44 @@ var express = require('express');
 var router = express.Router();
 
 var Category = require('../models/Category');
+var Content = require('../models/Content');
 
 
 //可以使用子路由
 //这里路径为 /user
 router.get('/',function (req, res, next) {
-    //读取所有的分类信息
-    //sort进行排序，默认的id中包含了时间戳，所以生成的id应该是递增的。-1表示降序
+
+    var data = {
+        userInfo:req.userInfo,
+        categories:[], //用于头部分类显示
+        page:Number(req.query.page || 1),
+        limit:5,
+        pages:0,
+        count:0,
+        contents:[]
+    };
+
     Category.find().sort({_id:-1}).then(function (categories) {
+        //读取所有的分类信息
+        data.categories = categories;
+        //读取内容数
+        return Content.count();
+    }).then(function (count) {
+        data.count = count;
+        //计算总页数。ceil 为向上取整
+        data.pages = Math.ceil(count / data.limit);
+        //取值不能超过 pages(总页数)
+        data.page = Math.min(data.page, data.pages);
+        //取值不能小于1
+        data.page = Math.max(data.page, 1);
 
-            res.render('main/index',{
-                userInfo:req.userInfo,
-                categories:categories
-            });
+        var skip = (data.page - 1) * data.limit;
 
+        return Content.find().sort({_id:-1}).limit(data.limit).skip(skip).populate(['category','user']);
+    }).then(function(contents) {
+        data.contents = contents;
+        console.log(data);
+        res.render('main/index', {data:data});
     })
 
 });
